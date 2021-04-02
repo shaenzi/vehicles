308539ecc58ef783e0a99faabb9c3030a4ec5558
Sara Hänzi <sarahaenzi@gmail.com>
2021-04-02 20:48:24 +0200

    add more missing selfs (arg for every class method!)

--- robot_classes.py
+++ robot_classes.py
@@ -8,10 +8,10 @@ class BasicVehicle():
         pass
         # TODO: have a robot class here, evaluate it at regular intervals
 
-    def start_timer():
+    def start_timer(self):
         pass
 
-    def stop_timer():
+    def stop_timer(self):
         pass
 
 class Vehicle(BasicVehicle):
@@ -19,17 +19,18 @@ class Vehicle(BasicVehicle):
         self.type = type
         # TODO add stuff for different types 
 
-    def stop():
+    def stop(self):
         pass
 
-    def start():
+    def start(self):
         pass
 
-    def quit():
+    def quit(self):
         pass
 
 class Robot:
     def __init__(self):
+        # initialise i2c and sensor
         self.i2c = busio.I2C(board.SCL, board.SDA)
         self.i2c.init(board.SCL, board.SDA, 400000) #last argument is frequency
         #i2c.scan returns 57, which is 0x39, which is the address of the sensor on the bus
@@ -38,15 +39,15 @@ class Robot:
         self.sensor.enable_proximity = True
 
 
-    def read():
+    def read(self):
         self.proximity = self.sensor.proximity
         r, g, b, c = self.sensor.color_data
         self.r, self.g, self.b, self.c = self._convert_color(r,g,b,c)
 
-    def print_values():
+    def print_values(self):
         print(f'proximity: {self.proximity}, brightness: {self.c}')
     
-    def set():
+    def set(self):
         pass
         # TODO: set motor speeds
     
